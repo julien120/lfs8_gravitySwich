016e9e97d7dc6182e6bd39fc660b804915564a97
M-Yuki-Hasuo <huyuda1220@gmail.com>
2020-09-21 17:52:56 +0900

    add triggerSideWall, ScoreUI and movingPlayer

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool scoreStop;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(0.1f, 0, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 100f,0));
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.CompareTag("wallChecker"))
+        {
+            Debug.Log("当たった");
+            scoreStop = true;
+        }
+    }
+
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text scoreText;
+    float scoreCount;
+
+    PlayerController playerController;
+    public GameObject PlayerControllerObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       playerController = PlayerControllerObject.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Playerの座標をScoreに代入しているが、Scoreを時間にし、ゲームオーバーでそのまま泊まる方がいいかも
+        //scoreCount += playerController.gameObject.transform.position.x;
+
+        if(playerController.scoreStop == false) { 
+
+        scoreCount += Time.deltaTime;
+        scoreText.text = scoreCount.ToString("f0");
+        }
+    }
+}
